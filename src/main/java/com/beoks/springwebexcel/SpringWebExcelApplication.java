c9ccd9ced91712133de63b806bb7dac666252297
Zayden(이재성) <leejs@gabia.com>
2024-03-21 10:03:07 +0900

    initial commit

---
+++ src/main/java/com/beoks/springwebexcel/SpringWebExcelApplication.java
@@ -0,0 +1,13 @@
+package com.beoks.springwebexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebExcelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beoks/springwebexcel/SpringWebExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beoks.springwebexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
